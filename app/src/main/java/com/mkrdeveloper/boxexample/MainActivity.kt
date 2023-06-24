34c110e1614c24bd083e9a91c50b270d74c6be95
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-06-24 14:56:07 +0200

    first init

---
+++ app/src/main/java/com/mkrdeveloper/boxexample/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.mkrdeveloper.boxexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mkrdeveloper.boxexample.ui.theme.BoxExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Box(modifier = Modifier
+        .background(Color.White)
+        .fillMaxSize(),
+    contentAlignment = Alignment.Center
+    ) {
+
+        Box(modifier = Modifier
+            .background(Color.Yellow)
+            .width(250.dp)
+            .height(250.dp)
+        , contentAlignment = Alignment.BottomCenter
+        ) {
+
+            Box(modifier = Modifier
+                .background(Color.Green)
+                ) {
+
+                Text(text = "Subscribe please", fontSize = 38.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    MyApp()
+}
